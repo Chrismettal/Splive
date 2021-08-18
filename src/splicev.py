40c7c3e1d015e69138be4bc2eb503d0fbfbb4205
Chrismettal <chrismettal@binary-6.de>
2021-08-18 18:42:49 +0200

    Initial commit

---
+++ src/splicev.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#############################################################################
+##                               SpliceV                                   ##
+#############################################################################
+# Command line tool to append several csv files together into one large file.
+# Mainly used to concat several daily logfiles into one for better 
+# visualization
+
+import os
+import argparse
+
+#############################################################################
+##                           Global constants                              ##
+#############################################################################
+CLIENT_ID = "844fde0b2950ccf35329"  # The ID of this app for authorization
+
+#############################################################################
+##                               Helpers                                   ##
+#############################################################################
+
+print("One smart fellow he smelled fart DAMMIT")
