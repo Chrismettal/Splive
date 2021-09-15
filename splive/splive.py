cc46d6b2cb11059f448a503e53b14ebedd3d14be
Chrismettal <chrismettal@binary-6.de>
2021-09-15 12:40:33 +0200

    Fix typo

--- splive/splive.py
+++ splive/splive.py
@@ -164,10 +164,10 @@ def main():
     for file in file_path_list:
         if file == output_file:
             file_path_list.remove(file)
-            pring("Ignored output file:")
+            print("Ignored output file:")
 
         print(file)
-        
+
     print("---")
 
     # Create spliced file
